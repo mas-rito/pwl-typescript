6997cd9e52b63daf5829103d5e6cfe5949af095c
Rito Ramadhan <126365429+mas-rito@users.noreply.github.com>
2026-02-25 04:46:43 +0000

    update name in index

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ function greet(name: string, umur: number): string {
     return `Selamat datang, ${name} di PWL, umur saya ${umur}`;
 }
 
-const mahasiswa = "Budi";
+const mahasiswa = "Rito Ramadhan";
 const umur : number = 19
 console.log(greet(mahasiswa, umur));
